25337403a6836535260377b7b8683a9b914a0017
Lounes BEHLOUL <lounes.behloul@javista.com>
2023-03-12 19:28:30 +0100

    Saigo no push

--- tests/siteManipulation.spec.ts
+++ tests/siteManipulation.spec.ts
@@ -4,8 +4,10 @@ test('Search feature', async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc/');
   await page.goto('https://demo.playwright.dev/todomvc/#/');
   await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Recherche avancée' }).click();
+  await page.getByRole('checkbox').check();
   await page.getByPlaceholder('Rechercher').click();
-  await page.getByPlaceholder('Rechercher').fill('bli');
+  await page.getByPlaceholder('Rechercher').fill('blin');
   await page.getByRole('link', { name: 'View Details' }).click();
 });
 
@@ -15,4 +17,14 @@ test('test inexistant artwork', async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await page.goto('http://localhost:3000/artworks/54');
   await page.goto('http://localhost:3000/artworks/-54');
+});
+
+test('Advanced search feature', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/');
+  await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Recherche avancée' }).click();
+  await page.getByRole('checkbox').check();
+  await page.getByLabel('Artiste ou culture').click();
+  await page.getByLabel('Artiste ou culture').fill('James');
 });
